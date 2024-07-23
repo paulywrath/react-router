0b796a38a0748f3d0bfe93638d29854a5191f01b
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-23 14:53:41 -0400

    Add "Back to Home" button

--- src/components/OtherColor.jsx
+++ src/components/OtherColor.jsx
@@ -1,11 +1,18 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 function OtherColor() {
   
   const {color} = useParams();
   
+  const navigate = useNavigate();
+
+  const backToHome = () => {
+    navigate('/')
+  }
+
   return (
     <>
+      <button onClick={backToHome}>Back to Home</button>
       <h2>{color}</h2>
     </>
   )
